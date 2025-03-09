ef546c560d9eab5f90767c63766976ddd37148f4
bedirhanarvas <96867288+bedirhanarvas@users.noreply.github.com>
2025-03-09 14:01:27 +0300

    Add files via upload

---
+++ Inheritance/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Customer customer = new Customer();
+		CustomerManager customerManager = new CustomerManager();
+		customerManager.List();
+		
+		
+	}
+
+}
